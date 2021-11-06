728b955c439d3619c3312c790627521cba5740c9
Taichi Masuyama <montanha.masu536@gmail.com>
2021-11-06 13:46:59 +0000

    Modified mongoose settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,24 @@
 import {join} from 'path';
 import {loggerConfig} from './logger';
-import mongooseConfig from './mongoose';
+import { isProduction } from './env';
 
 const {version} = require('../../package.json');
+
 export const rootDir = join(__dirname, '..');
 
 export const config: Partial<TsED.Configuration> = {
   version,
   rootDir,
   logger: loggerConfig,
-  mongoose: mongooseConfig,
+  mongoose: [{
+    id: 'tsed-mongo-sdk',
+    url: process.env.MONGO_URI || 'mongodb://mongo:27017/tsed-mongo-sdk',
+    connectionOptions: {
+      dbName: process.env.MONGO_DB_NAME || 'mongo',
+      user: process.env.MONGO_USER_NAME || 'mongo',
+      pass: process.env.MONGO_USER_NAME || 'changeme',
+      autoIndex: !isProduction,
+    }
+  }],
   // additional shared configuration
 };
--- src/config/mongoose/default.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  id: 'tsed-mongo-sdk',
-  url: process.env.MONGO_URI || 'mongodb://mongo:27017/tsed-mongo-sdk',
-  connectionOptions: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-};
